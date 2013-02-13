2fece9572222cdf08ced9bc42365e881b39d7a59
Alex Lau <laux0083@gmail.com>
2013-02-13 15:22:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datamove::Application.config.secret_token = 'fc693595e691f9b54a235c8cd44f844c2ab889947bed06492cd32877b7d44c2b1ff05ca253db3241e8338f15e438dfe3f32c1d3b86afe8e5f67f2434d0e5a228'
