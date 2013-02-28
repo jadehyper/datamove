a44c9a89ecd834a00fe26e914ae9a89f069d5bc2
Alex Lau <laux0083@gmail.com>
2013-02-28 09:54:26 -0600

    added table definition to analyze screen and display data in html table

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -1,9 +1,13 @@
 class ImportController < ApplicationController
   def analyze
+    table_name = params[:temp_db_table]
 
-    if params[:temp_db_table]
-      @temp_db_data = TempDb.get_data(params[:temp_db_table])
+
+    if table_name
+      @temp_db_data = TempDb.get_data(table_name)
+      @table_def = TempDb.get_definition(table_name)
     end
+
 
   end
 end
