0f8ef8d497a0d8da2609893d7277415b7c668515
Alex Lau <laux0083@gmail.com>
2013-03-22 14:01:22 -0500

    escaping javascript when return html in ajax

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -29,10 +29,22 @@ class ImportController < ApplicationController
 
   def temp_tab_cols
     temp_table_name = params[:temp_db_table]
+
+    @temp_table_def = []
     if temp_table_name
       @temp_db_data = TempDb.get_data(temp_table_name)
-      @temp_table_def = TempDb.get_definition(temp_table_name)
+      all_column_defs = TempDb.get_definition(temp_table_name)
+
+      all_column_defs.each do |col_def|
+        @temp_table_def << col_def[0]
+      end
     end
+
+    # @temp_table_def = @temp_table_def.to_a
+    logger.debug @temp_table_def
+    #logger.debug @temp_table_def[0]
+
+    #@temp_table_def = @temp_table_def[0]
 
     respond_to do |format|
       format.js
